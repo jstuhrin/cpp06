86c98fa664659a3f8338df43b62bc942760575a0
Jan-Hendrik Stuhring <jstuhrin@f1r6s1.codam.nl>
2026-04-30 15:57:47 +0200

    implemented input validatoin

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,28 +1,84 @@
 
 #include "ScalarConverter.hpp"
 
+namespace
+{
+  void traverseNum(const std::string& input, std::size_t& i)
+  {
+    if (input[i] == '-' || input[i] == +)
+    {
+      ++i;
+    }
+    while (std::isdigit(input[i]))
+    {
+      ++i;
+    }
+    if (input[i] == '.')
+    {
+      ++i;
+    }
+    while (std::isdigit(input[i]))
+    {
+      ++i;
+    }
+  }
+
+  bool isFloat(const std::string& input)
+  {
+    if (input.back() != 'f')
+    {
+      return false;
+    }
+    std::size_t i = 0;
+    traverseNum(input, i);
+    if (i != input.size() - 2)
+    {
+      return false;
+    }
+    return true;
+  }
+
+  bool isDouble(const std::string& input)
+  {
+    std::size_t i = 0;
+    traverseNum(input, i);
+    if (i != input.size() - 1)
+    {
+      return false;
+    }
+    return true;
+  }
+
+  bool isInt(const std::string& input)
+  {
+    std::size_t i = 0;
+    traverseNum(input, i);
+    if (i != input.size() - 1)
+    {
+      return false;
+    }
+    return true;
+  }
+
+  bool isChar(const std::string& input)
+  {
+    if (!isalpha(input[0]))
+    {
+      return false;
+    }
+    if (input.size() > 1)
+    {
+      return false;
+    }
+    return true;
+  }
+}
+
 void ScalarConverter::convert(std::string input)
 {
-  std::stringstream ss;
-  ss.str(input);
-  char c;
-  ss >> c;
-  ss.clear();
 
-  ss.str(input);
-  int i;
-  ss >> i;
-  ss.clear();
 
-  ss.str(input);
-  float f;
-  ss >> f;
-  ss.clear();
 
-  ss.str(input);
-  double d;
-  ss >> d;
-  ss.clear();
 
   std::cout << "char: " << c << "\nint: " << i << "\nfloat: " << f << "\ndouble: " << d << "\n";
 }
